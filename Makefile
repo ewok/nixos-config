6faf9da9ae01be1f18848efd3436a9d3004e1b66
Artur Taranchiev <ewok@ewok.ru>
2020-12-21 09:51:57 +0300

    Change wrapper script format

--- Makefile
+++
@@ -1,5 +0,0 @@
-build:
-	nixos-rebuild build --flake ".$(m)"
-
-switch:
-	sudo nixos-rebuild switch --flake ".$(m)"
--- shell.nix
+++ shell.nix
@@ -1,10 +1,48 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.git
-    pkgs.git-crypt
-    pkgs.nixUnstable
-    pkgs.gnumake
-  ];
-}
+with pkgs;
+let
+  nix-switch = writeShellScriptBin "nix-switch" ''
+    if [ "$#" -eq 0 ]; then
+      sudo nixos-rebuild switch --flake "."
+    else
+      sudo nixos-rebuild switch --flake ".#$1" $2
+    fi
+  '';
+
+  nix-rebuild = writeShellScriptBin "nix-rebuild" ''
+    if [ "$#" -eq 0 ]; then
+      sudo nixos-rebuild build --flake "."
+    else
+      sudo nixos-rebuild build --flake ".#$1" $2
+    fi
+  '';
+
+  nix-rebuild-vm = writeShellScriptBin "nix-rebuild-vm" ''
+    if [ "$#" -eq 0 ]; then
+      sudo nixos-rebuild build-vm --flake "."
+    else
+      sudo nixos-rebuild build-vm --flake ".#$1" $2
+    fi
+  '';
+
+  nix = writeShellScriptBin "nix" ''
+    ${pkgs.nixFlakes}/bin/nix --option experimental-features "nix-command flakes ca-references" "$@"
+  '';
+
+in
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+      git
+      git-crypt
+
+      nix
+      nix-switch
+      nix-rebuild
+      nix-rebuild-vm
+    ];
+
+    shellHook = ''
+      PATH=${pkgs.nix}/bin:$PATH
+    '';
+  }
