f53b267a72bdd0f40e3563fa8c06f3031d0424af
Artur Taranchiev <ewok@ewok.ru>
2020-12-21 09:51:36 +0300

    Add gvfs for pcmanfm

--- modules/gui/misc/pcmanfm.nix
+++ modules/gui/misc/pcmanfm.nix
@@ -6,11 +6,13 @@ let
 in
   {
     config = mkIf gui.enable {
+      environment.systemPackages = with pkgs; [ lxqt.lxqt-policykit ];
+      networking.firewall.extraCommands = "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
+      services.gvfs.enable = true;
       home-manager.users.${username} = {
         home.packages = with pkgs; [
           pcmanfm
         ];
-
         xdg.configFile."pcmanfm/default".source = ./pcmanfm;
       };
     };
