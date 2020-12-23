b0ac580fc3bb2e03862e925e59ed1a676425d2a1
Artur Taranchiev <ewok@ewok.ru>
2020-12-23 23:37:49 +0300

    Update i3 config

--- modules/gui/wm/default.nix
+++ modules/gui/wm/default.nix
@@ -3,7 +3,40 @@ with lib;
 let
   gui = config.modules.gui;
   username = config.properties.user.name;
-  blurlock = config.home-manager.users.${username}.xdg.configHome + "/i3/blurlock";
+  blurlock = pkgs.writeShellScriptBin "blurlock" ''
+    RESOLUTION=$(${pkgs.xorg.xrandr}/bin/xrandr -q|sed -n 's/.*current[ ]\([0-9]*\) x \([0-9]*\),.*/\1x\2/p')
+    ${pkgs.imagemagick}/bin/import -silent -window root jpeg:- | ${pkgs.imagemagick}/bin/convert - -scale 20% -blur 0x2.5 -resize 500% RGB:- | \
+      ${pkgs.i3lock}/bin/i3lock --raw $RESOLUTION:rgb -i /dev/stdin -e $@
+  '';
+  i3exit = pkgs.writeShellScriptBin "i3exit" ''
+    case "$1" in
+      lock)
+          ${blurlock}/bin/blurlock
+          ;;
+      logout)
+          i3-msg exit
+          ;;
+      switch_user)
+          dm-tool switch-to-greeter
+          ;;
+      suspend)
+          ${blurlock}/bin/blurlock && systemctl suspend
+          ;;
+      hibernate)
+          ${blurlock}/bin/blurlock && systemctl hibernate
+          ;;
+      reboot)
+          systemctl reboot
+          ;;
+      shutdown)
+          systemctl poweroff
+          ;;
+      *)
+          echo "== ! i3exit: missing or invalid argument ! =="
+          echo "Try again with: lock | logout | switch_user | suspend | hibernate | reboot | shutdown"
+          exit 2
+    esac
+    '';
 in
   {
     config = mkIf gui.enable {
@@ -30,85 +63,131 @@ in
       home-manager.users.${username} = {
 
         home.packages = with pkgs; [
-          i3status-rust
-          xdotool
-          xorg.xwininfo
           imagemagick
-          xorg.xrandr
+          xdotool
           xkb-switch
+          xorg.xrandr
+          xorg.xwininfo
+
+          blurlock
+          i3exit
         ];
 
+
         xdg.configFile."i3/config".source = ./config/config;
-        xdg.configFile."i3/mc-win-center.sh".source = ./config/mc-win-center.sh;
 
-        xdg.configFile."i3/blurlock" = {
-          text = ''
-            #!${pkgs.bash}/bin/bash
-            RESOLUTION=$(${pkgs.xorg.xrandr}/bin/xrandr -q|sed -n 's/.*current[ ]\([0-9]*\) x \([0-9]*\),.*/\1x\2/p')
-            ${pkgs.imagemagick}/bin/import -silent -window root jpeg:- | ${pkgs.imagemagick}/bin/convert - -scale 20% -blur 0x2.5 -resize 500% RGB:- | \
-              ${pkgs.i3lock}/bin/i3lock --raw $RESOLUTION:rgb -i /dev/stdin -e $@
-          '';
-          executable = true;
-        };
-
-        xdg.configFile."i3/i3exit" = {
-          text = ''
-            #!/usr/bin/env sh
-            case "$1" in
-              lock)
-                  ${blurlock}
-                  ;;
-              logout)
-                  i3-msg exit
-                  ;;
-              switch_user)
-                  dm-tool switch-to-greeter
-                  ;;
-              suspend)
-                  ${blurlock} && systemctl suspend
-                  ;;
-              hibernate)
-                  ${blurlock} && systemctl hibernate
-                  ;;
-              reboot)
-                  systemctl reboot
-                  ;;
-              shutdown)
-                  systemctl poweroff
-                  ;;
-              *)
-                  echo "== ! i3exit: missing or invalid argument ! =="
-                  echo "Try again with: lock | logout | switch_user | suspend | hibernate | reboot | shutdown"
-                  exit 2
-            esac
-          '';
-          executable = true;
-        };
-
-        xdg.configFile."i3status-rust/config.toml".source = ./config/i3status;
-
-        gtk = {
+        programs.i3status-rust = {
           enable = true;
-          font = {
-            name = "Noto Sans 10";
-          };
-          iconTheme = {
-            name = "Papirus";
-            package = pkgs.papirus-icon-theme;
-          };
-          theme = {
-            name = "Adapta";
-            package = pkgs.adapta-gtk-theme;
-          };
-          gtk3.extraConfig = {
-            gtk-application-prefer-dark-theme = true;
-          };
-        };
+          bars.default = {
+            blocks = [
+               {
+                 block = "time";
+                 interval = 60;
+                 format = "MSK: %a %d/%m %R";
+                 timezone = "Europe/Moscow";
+               }
+               {
+                 block = "time";
+                 interval = 60;
+                 format = "LDN: %R";
+                 timezone = "Europe/London";
+               }
+               {
+                 block = "time";
+                 interval = 60;
+                 format = "NY: %R";
+                 timezone = "America/New_York";
+               }
+               {
+                 block = "time";
+                 interval = 60;
+                 format = "PST: %R";
+                 timezone = "America/Los_Angeles";
+               }
+               {
+                 block = "disk_space";
+                 path = "/";
+                 alias = "/";
+                 info_type = "available";
+                 unit = "GB";
+                 interval = 60;
+                 warning = 20.0;
+                 alert = 10.0;
+               }
+               {
+                 block = "disk_space";
+                 path = "/mnt/Data";
+                 alias = "/Data";
+                 info_type = "available";
+                 unit = "GB";
+                 interval = 60;
+                 warning = 10.0;
+                 alert = 5.0;
+               }
+               {
+                 block = "cpu";
+                 interval = 1;
+               }
+               {
+                 block = "memory";
+                 display_type = "memory";
+                 format_mem = "{Mum}MB({Mup}%)";
+                 format_swap = "{SUm}MB({SUp}%)";
+                 icons = true;
+                 clickable = true;
+                 interval = 10;
+                 warning_mem = 80;
+                 warning_swap = 80;
+                 critical_mem = 95;
+                 critical_swap = 95;
+               }
+               {
+                 block = "music";
+                 player = "spotify";
+                 buttons = ["play" "next"];
+               }
+               {
+                 block = "sound";
+                 step_width = 5;
+               }
+               {
+                 block = "battery";
+                 interval = 60;
+                 format = "{percentage}% {time}";
+               }
+               {
+                 block = "custom";
+                 command = "xkb-switch";
+                 interval = 5;
+               }
+             ];
+             icons = "awesome";
+             theme = "bad-wolf";
+           };
+         };
 
-        services.screen-locker = {
-          enable = true;
-          lockCmd = "${pkgs.i3lock}/bin/i3lock -n -c 000000";
-        };
-      };
-    };
-  }
+         gtk = {
+           enable = true;
+           font = {
+             name = "Noto Sans 10";
+           };
+           iconTheme = {
+             name = "Papirus";
+             package = pkgs.papirus-icon-theme;
+           };
+           theme = {
+             name = "Adapta";
+             package = pkgs.adapta-gtk-theme;
+           };
+           gtk3.extraConfig = {
+             gtk-application-prefer-dark-theme = true;
+           };
+         };
+
+         services.screen-locker = {
+           enable = true;
+           lockCmd = "${blurlock}/bin/blurlock";
+         };
+       };
+     };
+   }
