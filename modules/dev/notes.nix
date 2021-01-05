42f02e68535de2e6e0793a8ebf743409f7bcfc56
Artur Taranchiev <ewok@ewok.ru>
2021-01-06 00:56:04 +0300

    Use patched sparkleshare

--- modules/dev/notes.nix
+++ modules/dev/notes.nix
@@ -1,14 +1,18 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 with lib;
 let
   dev = config.modules.dev;
   gui = config.modules.gui;
   username = config.properties.user.name;
+  mypkgs = import inputs.my-nixpkgs ({
+    config = config.nixpkgs.config;
+    localSystem = { system = "x86_64-linux"; };
+  });
 in
 {
   config = mkIf (dev.enable && gui.enable) {
     home-manager.users."${username}" = {
-      home.packages = [ pkgs.sparkleshare pkgs.git-lfs ];
+      home.packages = [ mypkgs.sparkleshare pkgs.git-lfs ];
     };
   };
 }
