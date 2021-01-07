3edf35b0dbc12fe6f9fbe69f36507629c1b38d26
Artur Taranchiev <ataranchiev@five9.com>
2021-01-08 00:46:04 +0300

    Add fenv to fish

--- modules/base/fish/default.nix
+++ modules/base/fish/default.nix
@@ -107,6 +107,15 @@ in
               sha256 = "7pR5/aQCkHct9lBx3u3nHmCAuo/V7XN1lC+ZvlRnNCo=";
             };
           }
+          {
+            name = "fenv";
+            src = pkgs.fetchFromGitHub {
+              owner = "oh-my-fish";
+              repo = "plugin-foreign-env";
+              rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+              sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+            };
+          }
         ];
 
         interactiveShellInit = ''
