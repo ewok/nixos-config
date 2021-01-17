551329b326a4a33756760e57bfae4f36220b6fa4
Artur Taranchiev <ewok@ewok.ru>
2021-01-17 19:37:49 +0300

    Fix usb failure on P52

--- modules/system/powermanagement.nix
+++ modules/system/powermanagement.nix
@@ -7,6 +7,8 @@ in
 {
   options.modules.system.powermanagement = {
     enable = mkEnableOption "Enable powermanagement.";
+    # Buggy on P52
+    powertop.enable = mkEnableOption "Enable powertop.";
     governor = mkOption {
       type = types.str;
       default = "powersave";
@@ -17,7 +19,7 @@ in
   config = mkIf cfg.enable {
     powerManagement = {
       enable = true;
-      powertop.enable = true;
+      powertop.enable = cfg.powertop.enable;
       cpuFreqGovernor = lib.mkDefault cfg.governor;
     };
     services.tlp.enable = true;
