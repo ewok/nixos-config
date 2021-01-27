848dabf8def32340eb47cc067dca6a356275ca90
Artur Taranchiev <ataranchiev@five9.com>
2021-01-27 17:39:56 +0300

    Update secrets

--- machines/working/default.nix
+++ machines/working/default.nix
@@ -55,7 +55,4 @@ in
   modules.communication.enable = true;
   modules.system.powermanagement.enable = true;
   modules.system.powermanagement.powertop.enable = true;
-
-  modules.mail.enable = true;
-  modules.mail.accounts = properties.email.accounts;
 }
