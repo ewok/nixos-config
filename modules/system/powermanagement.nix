826329c2729e66d21b8e7c34523bb6de5feb2d0c
Artur Taranchiev <ewok@ewok.ru>
2021-02-19 21:09:33 +0300

    Update

--- modules/system/powermanagement.nix
+++ modules/system/powermanagement.nix
@@ -78,8 +78,8 @@ in
       services.actkbd = {
         enable = true;
         bindings = [
-          { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 5"; }
-          { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 5"; }
+          { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 1"; }
+          { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 1"; }
         ];
       };
       users.users.${username}.extraGroups = [ "video" ];
