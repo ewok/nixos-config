6eb486358a0894b185acff0737431b5a48dbf0ec
Artur Taranchiev <ewok@ewok.ru>
2021-02-23 21:26:38 +0300

    Add keepasxc config

--- modules/gui/passwords.nix
+++ modules/gui/passwords.nix
@@ -15,6 +15,40 @@ in
           enpass
           keepassxc
         ];
+
+        xdg.configFile."keepassxc/keepassxc.ini".text = ''
+          [General]
+          BackupBeforeSave=true
+          ConfigVersion=1
+          GlobalAutoTypeKey=63
+          GlobalAutoTypeModifiers=100663296
+          MinimizeAfterUnlock=true
+
+          [Browser]
+          CustomProxyLocation=
+          Enabled=false
+
+          [GUI]
+          ApplicationTheme=dark
+          CompactMode=false
+          HidePasswords=true
+          MinimizeOnClose=true
+          MinimizeOnStartup=false
+          MinimizeToTray=true
+          ShowTrayIcon=true
+          TrayIconAppearance=monochrome-light
+
+          [KeeShare]
+          Active="<?xml version=\"1.0\"?>\n<KeeShare xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n  <Active/>\n</KeeShare>\n"
+          Foreign="<?xml version=\"1.0\"?>\n<KeeShare xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n  <Foreign/>\n</KeeShare>\n"
+          Own="<?xml version=\"1.0\"?>\n<KeeShare xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n  <PrivateKey/>\n  <PublicKey/>\n</KeeShare>\n"
+          QuietSuccess=true
+
+          [PasswordGenerator]
+          AdditionalChars=
+          ExcludedChars=
+          Logograms=true
+        '';
       };
     };
   }
