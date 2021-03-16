17dfd68f95d4c52fe52183b2fe35eacfe9fb62f2
Artur Taranchiev <ewok@ewok.ru>
2021-03-17 01:52:08 +0300

    Use autocutsel instead of clipit

--- modules/gui/misc/clipit.nix
+++ modules/gui/misc/clipit.nix
@@ -3,43 +3,79 @@ with lib;
 let
   gui = config.modules.gui;
   username = config.properties.user.name;
+
+  autocutseld = pkgs.writeScript "autocutseld" ''
+    #!${pkgs.bash}/bin/bash
+    set -ex
+
+    ${pkgs.autocutsel}/bin/autocutsel -selection CLIPBOARD &
+    PID1=$!
+    ${pkgs.autocutsel}/bin/autocutsel -selection PRIMARY &
+    PID2=$!
+
+    cleanup(){
+    echo "***Stopping"
+    kill -15 $PID1
+    wait $PID1
+    kill -15 $PID2
+    wait $PID2
+    }
+    trap 'cleanup' 1 2 3 6 15
+    wait $PID1
+    wait $PID2
+  '';
 in
   {
     config = mkIf gui.enable {
       home-manager.users.${username} = {
         home.packages = with pkgs; [
-          clipit
+          autocutsel
         ];
 
-        xdg.configFile."clipit/clipitrc" = {
-          text = ''
-            [rc]
-            use_copy=true
-            use_primary=true
-            synchronize=true
-            automatic_paste=false
-            show_indexes=false
-            save_uris=true
-            use_rmb_menu=false
-            save_history=false
-            history_limit=50
-            items_menu=20
-            statics_show=true
-            statics_items=10
-            hyperlinks_only=false
-            confirm_clear=false
-            single_line=true
-            reverse_history=false
-            item_length=50
-            ellipsize=2
-            history_key=<Ctrl><Alt>H
-            actions_key=<Ctrl><Alt>A
-            menu_key=<Ctrl><Alt>P
-            search_key=<Ctrl><Alt>F
-            offline_key=<Ctrl><Alt>O
-            offline_mode=false
-          '';
-        };
+          systemd.user.services.autocutseld = {
+            Unit = {
+              Description = "auto synchronize clipboards";
+              After = [ "graphical-session-pre.target" ];
+              PartOf = [ "graphical-session.target" ];
+            };
+
+            Service = {
+              Type = "simple";
+              ExecStart = "${autocutseld}";
+              Restart = "on-failure";
+            };
+
+            Install = { WantedBy = [ "graphical-session.target" ]; };
+          };
+        # xdg.configFile."clipit/clipitrc" = {
+        #   text = ''
+        #     [rc]
+        #     use_copy=true
+        #     use_primary=true
+        #     synchronize=true
+        #     automatic_paste=false
+        #     show_indexes=false
+        #     save_uris=true
+        #     use_rmb_menu=false
+        #     save_history=false
+        #     history_limit=50
+        #     items_menu=20
+        #     statics_show=true
+        #     statics_items=10
+        #     hyperlinks_only=false
+        #     confirm_clear=false
+        #     single_line=true
+        #     reverse_history=false
+        #     item_length=50
+        #     ellipsize=2
+        #     history_key=<Ctrl><Alt>H
+        #     actions_key=<Ctrl><Alt>A
+        #     menu_key=<Ctrl><Alt>P
+        #     search_key=<Ctrl><Alt>F
+        #     offline_key=<Ctrl><Alt>O
+        #     offline_mode=false
+        #   '';
+        # };
       };
     };
   }
