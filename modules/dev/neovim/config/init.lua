a728284335d7a7a3dc5008f0531254a12d38fbe2
Artur Taranchiev <ewok@ewok.ru>
2021-03-21 00:53:59 +0300

    nvim: configure snippets

--- modules/dev/neovim/config/init.lua
+++ modules/dev/neovim/config/init.lua
@@ -1760,9 +1760,10 @@
     packer.use {
       'hrsh7th/nvim-compe',
       requires = {
-        { 'honza/vim-snippets', opt = true },
-      --   { 'hrsh7th/vim-vsnip', opt = true },
-      --   { 'hrsh7th/vim-vsnip-integ', opt = true },
+        { 'honza/vim-snippets', },
+        { 'hrsh7th/vim-vsnip', },
+        { 'cstrap/python-snippets', },
+        { 'rust-lang/vscode-rust', },
       },
       config = function ()
         vim.cmd [[set shortmess+=c]]
@@ -1776,23 +1777,23 @@
           throttle_time = 80;
           source_timeout = 200;
           incomplete_delay = 400;
-          allow_prefix_unmatch = false;
-          max_abbr_width = 1000;
-          max_kind_width = 1000;
-          max_menu_width = 1000000;
+          max_abbr_width = 100;
+          max_kind_width = 100;
+          max_menu_width = 100;
           documentation = true;
 
           source = {
             path = true;
             buffer = true;
             calc = true;
-            vsnip = false;
+            vsnip = true;
             nvim_lsp = true;
             nvim_lua = true;
             spell = true;
             tags = true;
             snippets_nvim = true;
-            treesitter = true;
+            nvim_treesitter = true;
+            emoji = true;
           };
         }
 
@@ -1809,34 +1810,44 @@
           end
         end
 
-        -- Use (s-)tab to:
-        --- move to prev/next item in completion menuone
-        --- jump to prev/next snippet's placeholder
-        _G.tab_complete = function()
+        _G.tab_complete = function(key)
+          local key = key or "<Tab>"
           if vim.fn.pumvisible() == 1 then
             return t "<C-n>"
           elseif vim.fn.call("vsnip#available", {1}) == 1 then
             return t "<Plug>(vsnip-expand-or-jump)"
           elseif check_back_space() then
-            return t "<Tab>"
+            return t(key)
           else
             return vim.fn['compe#complete']()
           end
         end
-        _G.s_tab_complete = function()
+        _G.s_tab_complete = function(key)
+          local key = key or "<S-Tab>"
           if vim.fn.pumvisible() == 1 then
             return t "<C-p>"
           elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
             return t "<Plug>(vsnip-jump-prev)"
           else
-            return t "<S-Tab>"
+            return t(key)
           end
         end
 
-        vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-        vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-        vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-        vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+        map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+        map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+        map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+        map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+        map("i", "<C-j>", "v:lua.tab_complete('<C-j>')", {expr = true})
+        map("s", "<C-j>", "v:lua.tab_complete('<C-j>')", {expr = true})
+        map("i", "<C-k>", "v:lua.s_tab_complete('<C-k>')", {expr = true})
+        map("s", "<C-k>", "v:lua.s_tab_complete('<C-k>')", {expr = true})
+
+        map("i", "<C-Space>", "compe#complete()", {expr = true})
+        map("i", "<CR>", "compe#confirm('<CR>')", {expr = true})
+        map("i", "<C-e>", "compe#close('<C-e>')", {expr = true})
+        map("i", "<C-f>", "compe#scroll({ 'delta': +4 })", {expr = true})
+        map("i", "<C-b>", "compe#scroll({ 'delta': -4 })", {expr = true})
       end,
     }
   -- }}}
