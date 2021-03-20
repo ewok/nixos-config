cbe99170bc341787b310138aa4d4529260daff20
Artur Taranchiev <ewok@ewok.ru>
2021-03-20 22:16:38 +0300

    nvim: configure lspkind

--- modules/dev/neovim/config/init.lua
+++ modules/dev/neovim/config/init.lua
@@ -1847,7 +1847,36 @@
     packer.use {
       'neovim/nvim-lspconfig',
       requires = {
-        { 'onsails/lspkind-nvim', },
+        {
+          'onsails/lspkind-nvim',
+          config = function()
+            require('lspkind').init({
+              with_text = false,
+              symbol_map = {
+                Text = '  ',
+                Method = '  ',
+                Function = '  ',
+                Constructor = '  ',
+                Variable = '[]',
+                Class = '  ',
+                Interface = ' 蘒',
+                Module = '  ',
+                Property = '  ',
+                Unit = ' 塞 ',
+                Value = '  ',
+                Enum = ' 練',
+                Keyword = '  ',
+                Snippet = '  ',
+                Color = '',
+                File = '',
+                Folder = ' ﱮ ',
+                EnumMember = '  ',
+                Constant = '  ',
+                Struct = '  '
+              },
+            })
+          end,
+        },
         { 'glepnir/lspsaga.nvim',
         config = function ()
           map('n', 'K', [[<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>]], { noremap = true, silent = true })
