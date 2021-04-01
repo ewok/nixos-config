020a0ca45fdd27f8797821f78a777e29e572f608
Artur Taranchiev <ewok@ewok.ru>
2021-04-01 10:43:01 +0300

    nvim: Add nvimtree for tracking chages

--- modules/dev/neovim/config/init.lua
+++ modules/dev/neovim/config/init.lua
@@ -1483,6 +1483,56 @@
       end,
     }
   -- }}}
+  -- NvimTree {{{
+    -- Buggy yet. Added for tracking.
+    packer.use {
+      'kyazdani42/nvim-tree.lua',
+      requires = {{'kyazdani42/nvim-web-devicons'}},
+      config = function ()
+        vim.g.nvim_tree_width = 40
+        vim.g.nvim_tree_auto_close = 1
+        -- vim.g.nvim_tree_quit_on_open = 1
+        vim.g.nvim_tree_follow = 1
+        -- Forgets state
+        -- vim.g.nvim_tree_tab_open = 1
+        vim.g.nvim_tree_group_empty = 1
+        map('n', '<leader>oE', ':NvimTreeToggle<CR>', { noremap = true })
+        map('n', '<leader>fP', ':NvimTreeFindFile<CR>', { noremap = true })
+
+        local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+        vim.g.nvim_tree_bindings = {
+          -- ["<CR>"] = ":YourVimFunction()<cr>",
+          -- ["u"] = ":lua require'some_module'.some_function()<cr>",
+
+          -- default mappings
+          ["<CR>"]           = tree_cb("edit"),
+          ["o"]              = tree_cb("edit"),
+          ["<C-]>"]          = tree_cb("cd"),
+          ["C"]              = tree_cb("cd"),
+          ["v"]              = tree_cb("vsplit"),
+          ["s"]              = tree_cb("split"),
+          ["t"]              = tree_cb("tabnew"),
+          ["<BS>"]           = tree_cb("close_node"),
+          ["<S-CR>"]         = tree_cb("close_node"),
+          ["<Tab>"]          = tree_cb("preview"),
+          ["I"]              = tree_cb("toggle_ignored"),
+          ["H"]              = tree_cb("toggle_dotfiles"),
+          ["R"]              = tree_cb("refresh"),
+          ["a"]              = tree_cb("create"),
+          ["d"]              = tree_cb("remove"),
+          ["r"]              = tree_cb("rename"),
+          ["<C-r>"]          = tree_cb("full_rename"),
+          ["x"]              = tree_cb("cut"),
+          ["c"]              = tree_cb("copy"),
+          ["p"]              = tree_cb("paste"),
+          ["[c"]             = tree_cb("prev_git_item"),
+          ["]c"]             = tree_cb("next_git_item"),
+          ["u"]              = tree_cb("dir_up"),
+          ["q"]              = tree_cb("close"),
+        }
+      end,
+    }
+  --   -- }}}
   -- NERDTree {{{
     packer.use {
       'preservim/nerdtree',
