855243e877007b3929c69db47164bff55437e778
Artur Taranchiev <ewok@ewok.ru>
2021-04-06 16:02:43 +0300

    zoom: use version from master branch

--- modules/communication/zoom.nix
+++ modules/communication/zoom.nix
@@ -1,14 +1,20 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 with lib;
 let
   gui = config.modules.gui;
   comm = config.modules.communication;
   username = config.properties.user.name;
+  master = import inputs.master (
+    {
+      config = config.nixpkgs.config;
+      localSystem = { system = "x86_64-linux"; };
+    }
+  );
 in
 {
   config = mkIf (gui.enable && comm.enable) {
     home-manager.users.${username} = {
-      home.packages = with pkgs; [
+      home.packages = with master; [
         zoom-us
       ];
     };
