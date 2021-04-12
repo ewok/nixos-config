e9e9dcc2bdb18bbdafde9ec57543e0e914576b1c
Artur Taranchiev <ewok@ewok.ru>
2021-04-12 16:02:16 +0300

    nixos: remove cryptomator overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@ self: super:
 {
   sof-firmware = super.callPackage ./sof-firmware.nix {};
   # zeal = super.libsForQt5.callPackage ./zeal.nix { };
-  cryptomator = super.callPackage ./cryptomator.nix {};
   lbry = super.callPackage ./lbry.nix {};
   sparkleshare = super.callPackage ./sparkleshare.nix {};
   todofish = super.callPackage ./todofish.nix {};
