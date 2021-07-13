f52c0a5461113f37840f11d00c95bb44d8855497
Artur Taranchiev <ewok@ewok.ru>
2021-07-13 09:27:05 +0300

    nvim: switch to nightly

--- modules/dev/neovim/default.nix
+++ modules/dev/neovim/default.nix
@@ -19,7 +19,7 @@ let
 
   my-nvim = pkgs.symlinkJoin {
     name = "my-neovim";
-    paths = [ pkgs.neovim ];
+    paths = [ pkgs.neovim-nightly ];
     postBuild = ''
       ln -s $out/bin/nvim $out/bin/vim
       ln -s $out/bin/nvim $out/bin/vi
