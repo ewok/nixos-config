d9bd946c8668cf9de41ed0ee9874f31a80812f06
Artur Taranchiev <ewok@ewok.ru>
2021-08-22 13:50:06 +0300

    Displays update

--- machines/ewok-p52/default.nix
+++ machines/ewok-p52/default.nix
@@ -3,6 +3,58 @@ let
   properties = config.properties;
   username = config.properties.user.name;
   homeDirectory = config.home-manager.users.${username}.home.homeDirectory;
+  homeDisplay = {
+        fingerprint = {
+          DP-1 = "00ffffffffffff0010acf54142524442091f0103803c2278eab9a5a356509f260e5054a54b00714f8180a9c0d1c00101010101010101023a801871382d40582c450056502100001e000000ff00375756355138330a2020202020000000fc0044454c4c205332373231480a20000000fd00304b1e5312000a202020202020011302032bf14f90050403020716010611121513141f230907078301000065030c001000681a00000100304be62a4480a0703827403020350056502100001a011d8018711c1620582c250056502100009e011d007251d01e206e28550056502100001e8c0ad08a20e02d10103e9600565021000018000000000000000000000000f1";
+          DP-2 = "00ffffffffffff00220e483600000000251d0103805021782a4784aa524da026115054a54b00d1c0a9c081c0b3009500810081800101e77c70a0d0a0295030203a00204f3100001a000000fd001e4c1e5e22000a202020202020000000fc0048502045333434630a20202020000000ff00334351393337324b50480a20200140020322b14a901f041302031112015a67030c001000003c67d85dc401448000e2006b9f3d70a0d0a0155030203a00204f3100001a565e00a0a0a0295030203500204f3100001a483f403062b0324040c01300204f3100001e7d4b80a072b02d4088c83600204f3100001c023a801871382d40582c4500204f3100001e0000009a";
+        };
+        config = {
+          DP-2 = {
+            enable = true;
+            crtc = 0;
+            gamma = "1.0:1.0:1.0";
+            mode = "3440x1440";
+            position = "0x0";
+            primary = true;
+            rate = "60.00";
+          };
+          DP-1 = {
+            enable = true;
+            crtc = 2;
+            gamma = "1.0:1.0:1.0";
+            mode = "1920x1080";
+            position = "3440x0";
+            rate = "60.00";
+          };
+        };
+        hooks = {
+          postswitch = ''
+            echo "Xft.dpi: 105" | xrdb -merge
+            xrandr --output DP-1 --scale 1.25x1.25 --mode 1920x1080
+            i3-msg restart
+          '';
+        };
+      };
+  laptopDisplay = {
+        fingerprint = {
+          eDP-1-1 = "00ffffffffffff0009e59207000000002c1b0104a5221378021bb0a658559d260e5054000000010101010101010101010101010101019c3b803671383c403020360058c21000001aa82f803671383c403020360058c21000001a000000fe00424f452043510a202020202020000000fe004e5631353646484d2d4e36310a00ed";
+        };
+        config = {
+          eDP-1-1 = {
+            enable = true;
+            gamma = "1.0:1.0:1.0";
+            mode = "1920x1080";
+            position = "3440x0";
+            rate = "60.00";
+          };
+        };
+        hooks = {
+          postswitch = ''
+            echo "Xft.dpi: 115" | xrdb -merge
+            i3-msg restart
+          '';
+        };
+      };
 in
 {
   imports = [
@@ -54,80 +106,25 @@ in
       dpi = 110;
     };
     displayProfiles = builtins.toJSON {
-      "home-dp1" = {
-        fingerprint = {
-          DP-1 = "00ffffffffffff00220e483600000000251d0103805021782a4784aa524da026115054a54b00d1c0a9c081c0b3009500810081800101e77c70a0d0a0295030203a00204f3100001a000000fd001e4c1e5e22000a202020202020000000fc0048502045333434630a20202020000000ff00334351393337324b50480a20200140020322b14a901f041302031112015a67030c001000003c67d85dc401448000e2006b9f3d70a0d0a0155030203a00204f3100001a565e00a0a0a0295030203500204f3100001a483f403062b0324040c01300204f3100001e7d4b80a072b02d4088c83600204f3100001c023a801871382d40582c4500204f3100001e0000009a";
-        };
-        config = {
-          DP-1 = {
-            enable = true;
-            crtc = 0;
-            primary = true;
-            position = "0x0";
-            mode = "3440x1440";
-            gamma = "1.0:1.0:1.0";
-            rate = "60.00";
-          };
-        };
-        # hooks.postswitch = readFile ./work-postswitch.sh;
-      };
-      "home-hdm1" = {
-        fingerprint = {
-          HDMI-0 = "00ffffffffffff00220e483600000000251d0103805021782a4784aa524da026115054a54b00d1c0a9c081c0b3009500810081800101e77c70a0d0a0295030203a00204f3100001a000000fd001e4c1e5e22000a202020202020000000fc0048502045333434630a20202020000000ff00334351393337324b50480a20200140020322b14a901f041302031112015a67030c001000003c67d85dc401448000e2006b9f3d70a0d0a0155030203a00204f3100001a565e00a0a0a0295030203500204f3100001a483f403062b0324040c01300204f3100001e7d4b80a072b02d4088c83600204f3100001c023a801871382d40582c4500204f3100001e0000009a";
-        };
-        config = {
-          HDMI-0 = {
-            enable = true;
-            crtc = 0;
-            primary = true;
-            position = "0x0";
-            mode = "3440x1440";
-            gamma = "1.0:1.0:1.0";
-            rate = "60.00";
-          };
-        };
-        # hooks.postswitch = readFile ./work-postswitch.sh;
-      };
-      "home-both" = {
-        fingerprint = {
-          eDP-1-1 = "00ffffffffffff0009e59207000000002c1b0104a5221378021bb0a658559d260e5054000000010101010101010101010101010101019c3b803671383c403020360058c21000001aa82f803671383c403020360058c21000001a000000fe00424f452043510a202020202020000000fe004e5631353646484d2d4e36310a00ed";
-          DP-1 = "00ffffffffffff00220e483600000000251d0103805021782a4784aa524da026115054a54b00d1c0a9c081c0b3009500810081800101e77c70a0d0a0295030203a00204f3100001a000000fd001e4c1e5e22000a202020202020000000fc0048502045333434630a20202020000000ff00334351393337324b50480a20200140020322b14a901f041302031112015a67030c001000003c67d85dc401448000e2006b9f3d70a0d0a0155030203a00204f3100001a565e00a0a0a0295030203500204f3100001a483f403062b0324040c01300204f3100001e7d4b80a072b02d4088c83600204f3100001c023a801871382d40582c4500204f3100001e0000009a";
-        };
-        config = {
+      home.fingerprint = lib.mkMerge [
+        homeDisplay.fingerprint
+        laptopDisplay.fingerprint
+      ];
+      home.config = lib.mkMerge [
+        homeDisplay.config
+        laptopDisplay.config
+        {
           eDP-1-1 = {
-            enable = true;
-            gamma = "1.0:1.0:1.0";
-            mode = "1920x1080";
-            position = "3440x0";
-            rate = "60.00";
+            position = "5360x0";
           };
-          DP-1 = {
-            enable = true;
-            crtc = 0;
-            primary = true;
-            position = "0x0";
-            mode = "3440x1440";
-            gamma = "1.0:1.0:1.0";
-            rate = "60.00";
-          };
-        };
-        # hooks.postswitch = readFile ./work-postswitch.sh;
-      };
-      "laptop" = {
-        fingerprint = {
-          eDP-1-1 = "00ffffffffffff0009e59207000000002c1b0104a5221378021bb0a658559d260e5054000000010101010101010101010101010101019c3b803671383c403020360058c21000001aa82f803671383c403020360058c21000001a000000fe00424f452043510a202020202020000000fe004e5631353646484d2d4e36310a00ed";
-        };
-        config = {
-          eDP-1-1 = {
-            enable = true;
-            gamma = "1.0:1.0:1.0";
-            mode = "1920x1080";
-            position = "3440x0";
-            rate = "60.00";
-          };
-        };
-      };
+        }
+      ];
+      home.hooks = homeDisplay.hooks;
+
+      laptop = laptopDisplay;
+      home2 = homeDisplay;
     };
+
     longitude = properties.longitude;
     latitude = properties.latitude;
     day = 5000;
