7bcd1b81c0e833092db973bf742e5994551bfc30
Artur Taranchiev <a.taranchiev@fhl.world>
2024-04-04 18:29:08 +0600

    add vpn

---
+++ modules/vpn/default.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, utils, ... }:
+with lib;
+let
+  cfg = config.opt.openvpn;
+
+in
+{
+  options.opt = {
+    openvpn = {
+      enable = mkEnableOption "openvpn";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs;
+      [
+        openvpn
+        rofi
+      ];
+    home.file."bin/vpn" = {
+      source = ./config/vpn;
+      executable = true;
+    };
+  };
+}
