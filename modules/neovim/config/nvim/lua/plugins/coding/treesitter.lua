6500b18cb6e6ea2cf38b7b18af07d2efbb534ec9
Artur Taranchiev <a.taranchiev@fhl.world>
2024-04-18 12:47:31 +0600

    Add treesitter textobjects

--- modules/neovim/config/nvim/lua/plugins/coding/treesitter.lua
+++ modules/neovim/config/nvim/lua/plugins/coding/treesitter.lua
@@ -69,46 +69,147 @@ local md_rule = [[
 ]]
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        local query = require("vim.treesitter.query")
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        lazy = true,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+                            ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+                            ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+                            ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
-        configs.setup({
-            ensure_installed = { "markdown_inline", "markdown" },
-            ignore_install = {},
-            sync_install = false,
-            auto_install = true,
-            matchup = {
-                enable = false,
-            },
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = true,
-                disable = { "yaml", "python", "html", "vue" },
-            },
-            incremental_selection = {
-                enable = false,
-                keymaps = {
-                    init_selection = "<cr>",
-                    node_incremental = "<cr>",
-                    node_decremental = "<bs>",
-                    scope_incremental = "<tab>",
+                            -- works for javascript/typescript files (custom capture I created in after/queries/ecma/textobjects.scm)
+                            ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
+                            ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
+                            ["l:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
+                            ["r:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
+
+                            ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+                            ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+                            ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+                            ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+                            ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+                            ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+                            ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+                            ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+                            ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+                            ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+                            ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+                        },
+                    },
+                    swap = {
+                        enable = true,
+                        swap_next = {
+                            ["m>"] = "@parameter.inner", -- swap parameters/argument with next
+                            -- ["<leader>nm"] = "@function.outer", -- swap function with next
+                        },
+                        swap_previous = {
+                            ["m<"] = "@parameter.inner", -- swap parameters/argument with prev
+                            -- ["<leader>pm"] = "@function.outer", -- swap function with previous
+                        },
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+                        goto_next_start = {
+                            ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+                            ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+                            ["]c"] = { query = "@class.outer", desc = "Next class start" },
+                            ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+                            ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+
+                            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+                            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+                        },
+                        goto_next_end = {
+                            ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+                            ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+                            ["]C"] = { query = "@class.outer", desc = "Next class end" },
+                            ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+                            ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+                        },
+                        goto_previous_start = {
+                            ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+                            ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+                            ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+                            ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+                            ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+                        },
+                        goto_previous_end = {
+                            ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+                            ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+                            ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+                            ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+                            ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+                        },
+                    },
                 },
-            },
-            autotag = {
-                enable = true,
-            },
-            context_commentstring = {
-                enable = true,
-                enable_autocmd = false,
-            },
-        })
+            })
 
-        query.set("markdown", "highlights", md_rule)
-    end,
-    event = { "BufReadPre", "BufNewFile" },
+            local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+            vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+            vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects"
+        },
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            local query = require("vim.treesitter.query")
+
+            configs.setup({
+                ensure_installed = { "markdown_inline", "markdown" },
+                ignore_install = {},
+                sync_install = false,
+                auto_install = true,
+                matchup = {
+                    enable = false,
+                },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                    disable = { "yaml", "python", "html", "vue" },
+                },
+                incremental_selection = {
+                    enable = false,
+                    keymaps = {
+                        init_selection = "<cr>",
+                        node_incremental = "<cr>",
+                        node_decremental = "<bs>",
+                        scope_incremental = "<tab>",
+                    },
+                },
+                autotag = {
+                    enable = true,
+                },
+                context_commentstring = {
+                    enable = true,
+                    enable_autocmd = false,
+                },
+            })
+
+            query.set("markdown", "highlights", md_rule)
+        end,
+        event = { "BufReadPre", "BufNewFile" },
+    },
 }
