341caf3ef9c6cc4757b22627d0c21350c256485d
Artur Taranchiev <a.taranchiev@fhl.world>
2024-05-15 01:00:12 +0600

    nvim: update gpt config

--- modules/neovim/config/nvim/lua/plugins/coding/gpt.lua
+++ modules/neovim/config/nvim/lua/plugins/coding/gpt.lua
@@ -12,6 +12,10 @@ return {
         "GpNew",
         "GpVnew",
         "GpTabnew",
+        "GpImage",
+        "GpNextAgent",
+        "GpCodeReview",
+        "GpUnitTests"
     },
     init = function()
         local wk = require("which-key")
@@ -26,9 +30,9 @@ return {
         local md = { noremap = true, silent = true, nowait = true }
 
         map({ "n", "i" }, "<C-g>c", "<cmd>GpChatNew<cr>", md, "New Chat")
-        map({ "n", "i" }, "<C-g>c", "<cmd>GpChatNew<cr>", md, "New Chat")
         map({ "n", "i" }, "<C-g>t", "<cmd>GpChatToggle<cr>", md, "Toggle Chat")
         map({ "n", "i" }, "<C-g>f", "<cmd>GpChatFinder<cr>", md, "Chat Finder")
+        map({ "n", "i" }, "<C-g>i", "<cmd>GpImage<cr>", md, "Image")
         map("v", "<C-g>c", ":<C-u>'<,'>GpChatNew<cr>", md, "Visual Chat New")
         map("v", "<C-g>p", ":<C-u>'<,'>GpChatPaste<cr>", md, "Visual Chat Paste")
         map("v", "<C-g>t", ":<C-u>'<,'>GpChatToggle<cr>", md, "Visual Toggle Chat")
@@ -67,10 +71,60 @@ return {
         map("v", "<C-g>wr", ":<C-u>'<,'>GpWhisperRewrite<cr>", md, "Visual Whisper Rewrite")
         map("v", "<C-g>wa", ":<C-u>'<,'>GpWhisperAppend<cr>", md, "Visual Whisper Append (after)")
         map("v", "<C-g>wb", ":<C-u>'<,'>GpWhisperPrepend<cr>", md, "Visual Whisper Prepend (before)")
+
+        map( "v", "<leader>c1", ":<C-u>'<,'>GpCodeReview<cr>", md, "[gpt] Code review")
+        map( "v", "<leader>c2", ":<C-u>'<,'>GpUnitTests<cr>", md, "[gpt] Code Unit tests")
     end,
     config = function()
         local gp = require("gp")
         local conf = require("conf")
-        gp.setup({ openai_api_key = conf.openai_token })
+        gp.setup({
+            openai_api_key = conf.openai_token,
+            image_dir = "",
+            agents = {
+                {
+                    name = "CodeGPT4o",
+                    chat = false,
+                    command = true,
+                    model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
+                    system_prompt = "You are an AI working as a code editor.\n\n"
+                        .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+                        .. "START AND END YOUR ANSWER WITH:\n\n```",
+                },
+                {
+                    name = "ChatGPT4",
+                    chat = true,
+                    command = false,
+                    model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+                    system_prompt = "You are a general AI assistant.\n\n"
+                        .. "The user provided the additional info about how they would like you to respond:\n\n"
+                        .. "- If you're unsure don't guess and say you don't know instead.\n"
+                        .. "- Ask question if you need clarification to provide better answer.\n"
+                        .. "- Think deeply and carefully from first principles step by step.\n"
+                        .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+                        .. "- Use Socratic method to improve your thinking and coding skills.\n"
+                        .. "- Don't elide any code from your output if the answer requires coding.\n"
+                        .. "- Take a deep breath; You've got this!\n",
+                },
+            },
+            hooks = {
+                -- example of usig enew as a function specifying type for the new buffer
+                CodeReview = function(gp, params)
+                    local template = "I have the following code from {{filename}}:\n\n"
+                        .. "```{{filetype}}\n{{selection}}\n```\n\n"
+                        .. "Please analyze for code smells and suggest improvements."
+                    local agent = gp.get_chat_agent()
+                    gp.Prompt(params, gp.Target.enew("markdown"), nil, agent.model, template, agent.system_prompt)
+                end,
+                -- example of adding command which writes unit tests for the selected code
+                UnitTests = function(gp, params)
+                    local template = "I have the following code from {{filename}}:\n\n"
+                        .. "```{{filetype}}\n{{selection}}\n```\n\n"
+                        .. "Please respond by writing table driven unit tests for the code above."
+                    local agent = gp.get_command_agent()
+                    gp.Prompt(params, gp.Target.enew, nil, agent.model, template, agent.system_prompt)
+                end,
+            },
+        })
     end,
 }
