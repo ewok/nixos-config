427a815370770568b0f3da0c60602787c85104e1
Artur Taranchiev <a.taranchiev@fhl.world>
2024-05-22 23:58:28 +0600

    add tf

---
+++ modules/terraform.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.opt.tf;
+in
+{
+  options.opt.tf = {
+    enable = mkEnableOption "tf";
+  };
+
+  config = mkIf cfg.enable {
+
+    home.packages = with pkgs; [
+      tenv
+    ];
+
+  };
+}
