2ffde5adbe629bcc4e604aff6ce45ceddb390d4b
Artur Taranchiev <a.taranchiev@fhl.world>
2024-05-28 19:09:12 +0600

    move to flake for direnv

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "ewoks envs";
 
   inputs = {
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # Darwin
@@ -19,15 +18,16 @@
 
     # Android related
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-23.05";
+      url = "github:nix-community/nix-on-droid";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
       inputs.home-manager.follows = "home-manager";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, home-manager, nix-on-droid, darwin, ... }@inputs:
+  outputs = { self, home-manager, nix-on-droid, darwin, flake-utils, ... }@inputs:
+
     let
-      inherit (inputs.nixpkgs-unstable.lib) genAttrs;
       nixpkgsDefaults = {
         config = {
           allowUnfree = true;
@@ -111,5 +111,129 @@
             }
           ];
         };
-    };
+
+    } // flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default =
+        let
+          pkgs = import inputs.nixpkgs-unstable (nixpkgsDefaults // {
+            system = "${system}";
+          });
+        in
+        with pkgs; pkgs.mkShell {
+          packages =
+            let
+
+              nixosMy = writeShellScriptBin "n" ''
+                if [ "$#" -eq 0 ]; then
+                  echo "Provide a command as a first argument please."
+                  echo "Usual nixos-rebuild commands + clean, clean-result, update-nix, update-all, crypt"
+
+                elif [ "$#" -eq 1 ]; then
+
+                  if [ "$1" == "clean-result" ];then
+                    nix-store --gc --print-roots | awk '{print $1}' | grep '/result$' | sudo xargs rm
+
+                  elif [ "$1" == "crypt" ];then
+                    ${git-crypt-status}/bin/git-crypt-status
+
+                  elif [ "$1" == "clean" ];then
+                    nix-collect-garbage -d
+                    nix-store --gc
+                    sudo nix-collect-garbage -d || echo error running sudo
+                    sudo nix-store --gc || echo error running sudo
+
+                  # elif [ "$1" == "update-nix" ];then
+                  #   for flake in stable nixpkgs master home-manager;
+                  #   do
+                  #     nix flake update --update-input $flake
+                  #   done
+
+                  elif [ "$1" == "update-all" ];then
+                    # for flake in stable nixpkgs master nixos-hardware neovim-nightly-overlay home-manager;
+                    # do
+                      nix flake update
+                    # done
+
+                  elif [ "$1" == "switch" ];then
+                    sudo nixos-rebuild $1 --show-trace --verbose --flake "."
+                    # $${nix-copy-nas}/bin/nix-copy-nas /run/current-system
+                  else
+                    sudo nixos-rebuild $1 --verbose --flake "."
+                  fi
+
+                else
+                  sudo nixos-rebuild $1 --show-trace --verbose --flake ".#$2" $3
+                fi
+              '';
+
+              hmMy = writeShellScriptBin "hm" ''
+                if [ "$#" -eq 0 ]; then
+                  echo "Provide a command as a first argument please."
+                  echo "Usual h switch build"
+
+                elif [ "$#" -eq 1 ]; then
+                  home-manager --impure --flake "." $1
+                else
+                  home-manager --impure --flake ".#$2" $1
+                fi
+              '';
+
+              nix = writeShellScriptBin "nix" ''
+                ${pkgs.nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
+              '';
+
+              git-crypt-status = writeShellScriptBin "git-crypt-status" ''
+                git-crypt status -e
+              '';
+
+              n-darwin-build = writeShellScriptBin "n-darwin-build" ''
+                nix run nix-darwin -- build --flake '.#mac'
+              '';
+              n-darwin-switch = writeShellScriptBin "n-darwin-switch" ''
+                nix run nix-darwin -- switch --flake '.#mac'
+              '';
+              n-droid-build = writeShellScriptBin "n-droid-build" ''
+                nix-on-droid build --flake '.#android'
+              '';
+              n-droid-switch = writeShellScriptBin "n-droid-switch" ''
+                nix-on-droid switch --flake '.#android'
+              '';
+              n-steam-build = writeShellScriptBin "n-steam-build" ''
+                nix run home-manager -- build --flake '.#steamdeck'
+              '';
+              n-steam-switch = writeShellScriptBin "n-steam-switch" ''
+                nix run home-manager -- switch -b bakup --flake '.#steamdeck'
+              '';
+              n-orb-build = writeShellScriptBin "n-orb-build" ''
+                nix run home-manager -- build --flake '.#orb'
+              '';
+              n-orb-switch = writeShellScriptBin "n-orb-switch" ''
+                nix run home-manager -- switch -b bakup --flake '.#orb'
+              '';
+            in
+            [
+              git
+              git-crypt
+              git-crypt-status
+              nixosMy
+              hmMy
+              nixpkgs-fmt
+              neovim
+              nix
+              n-darwin-build
+              n-darwin-switch
+              n-droid-build
+              n-droid-switch
+              n-steam-build
+              n-steam-switch
+              n-orb-build
+              n-orb-switch
+            ];
+
+          shellHook = ''
+            export NIXPKGS_ALLOW_UNFREE=1
+            git-crypt-status
+          '';
+        };
+    });
 }
