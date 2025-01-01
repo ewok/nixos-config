0d73fc9d138a7ed99db077c35ed5fe157e09ea47
Artur Taranchiev <github@in.ewok.email>
2025-01-01 14:06:13 +0600

    feat(sway-config): enhance bindsyms and workspace setup

--- modules/hm/wm/sway.nix
+++ modules/hm/wm/sway.nix
@@ -26,5 +26,9 @@ in
       xdg.configFile."waybar/style.css" = {
         source = utils.templateFile "style.css" ./config/sway/style.css vars;
       };
+      xdg.configFile."sway/config.d/80-custom.conf".text = ''
+        for_window [title="Picture in picture"] floating enable, sticky enable
+        for_window [title="Picture-in-Picture"] floating enable, sticky enable
+      '';
     };
 }
